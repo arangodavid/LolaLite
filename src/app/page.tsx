bc452e0c8504b231686e581f766e2e0c84b2208b
David Arango <davidarango@MacBookPro.lan>
2026-04-19 00:35:28 -0400

    Dashboard skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
-import Image from "next/image";
+import Link from "next/link";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default function Dashboard() {
   return (
-    <main></main>
+    <main className={styles.dashboardMain}>
+      <header className={styles.dashboardHeader}>
+        <nav>
+          <ul>
+            <li><Link href="/">Dashboard</Link></li>
+            <li><Link href="/settings">Settings</Link></li>
+            <li><Link href="/logout">Logout</Link></li>
+          </ul>
+        </nav>
+        <h1 className={styles.dashboardTitle}>Lola-Lite</h1>
+      </header>
+      <section className={styles.dashboardContent}>
+        <section className={styles.preferences}>Preferences(placeholder)</section>
+        <Link className={styles.ctas} href="/upload-travels">+ Add to your travels</Link>
+        <section className={styles.recentMemories}>Recent memories(placeholder)</section>
+      </section>
+    </main>
   )  
 }
